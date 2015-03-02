395afa702061f0ac14eb4224ff8987613613585e
Ben-Grey <unil.greyeagle@gmail.com>
2015-03-02 14:50:21 -0800

    made subprocess multiplatform (win/lin)

--- pdf_extract.py
+++ pdf_extract.py
@@ -3,12 +3,6 @@ import argparse
 import subprocess
 
 
-def get_cover(path, pdf):
-    """ Takes the first page of the pdf and writes it to an image (cover.jpg)
-    """
-    subprocess.call('convert %s%s[0] %scover.jpg' % (path, pdf, path), shell=True)
-# identify working directory
-# identify OS (win/lin)
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description='Takes a pdf in and gets a cover image from the first page'
@@ -17,5 +11,12 @@ if __name__ == "__main__":
     parser.add_argument('description', metavar='FILE', default=None, help=
                         'description to use on pdf download page')
     args = parser.parse_args()
-    get_cover(r'd:\\', 'test.pdf')
 
+    # Identify OS
+    OS = platform.system()
+    if OS == 'Windows':
+        subprocess.call(["convert", join(path, args.pdf) + "[0]",
+                         join(path, "cover.jpg")], shell = True)
+    else:
+        subprocess.call("convert " + join(path, args.pdf) + "[0] " +
+                         join(path, "cover.jpg"), shell=True)
