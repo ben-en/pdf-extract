5d2d803f3f8f4fe8f24b7b1471a6e31a02ad3fbe
Ben-Grey <unil.greyeagle@gmail.com>
2015-03-02 15:04:16 -0800

    moved if __name__ == "__main__" and added functions

--- pdf_extract.py
+++ pdf_extract.py
@@ -15,6 +15,37 @@ def hash_url(url):
     return md5.hexdigest()
 
 
+def assemble_path(path, pdf):
+    # Assemble path
+    pdf_hash = hash_url(args.pdf)
+    path = join(getcwd(), pdf_hash)
+    if os.path.exists(path):
+        shutil.rmtree(path)
+        os.mkdir(path)
+    else:
+        os.mkdir(path)
+    shutil.move(join(original_dir, args.pdf), join(path, args.pdf))
+
+
+def get_cover(path, pdf):
+    # Identify OS
+    OS = platform.system()
+    if OS == 'Windows':
+        subprocess.call(["convert", join(path, args.pdf) + "[0]",
+                         join(path, "cover.jpg")], shell = True)
+    else:
+        subprocess.call("convert " + join(path, args.pdf) + "[0] " +
+                         join(path, "cover.jpg"), shell=True)
+
+
+def build_landing(path, pdf, desc):
+    # Build landing page
+    html = bottle.template('template', title=pdf[:-4], desc=desc)
+    with open(join(path, 'index.html'), 'w') as f:
+        f.write(html)
+    print html
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description='Takes a pdf in and gets a cover image from the first page'
@@ -28,28 +59,8 @@ if __name__ == "__main__":
     bottle.TEMPLATE_PATH.insert(0, original_dir)
     shell = True
 
+    assemble_path(path, pdf)
 
-    # Assemble path
-    pdf_hash = hash_url(args.pdf)
-    path = join(getcwd(), pdf_hash)
-    if os.path.exists(path):
-        shutil.rmtree(path)
-        os.mkdir(path)
-    else:
-        os.mkdir(path)
-    shutil.move(join(original_dir, args.pdf), join(path, args.pdf))
+    get_cover(path, pdf)
 
-    # Identify OS
-    OS = platform.system()
-    if OS == 'Windows':
-        subprocess.call(["convert", join(path, args.pdf) + "[0]",
-                         join(path, "cover.jpg")], shell = True)
-    else:
-        subprocess.call("convert " + join(path, args.pdf) + "[0] " +
-                         join(path, "cover.jpg"), shell=True)
-
-    # Build landing page
-    html = bottle.template('template', title=args.pdf[:-4], desc=args.description)
-    with open(join(path, 'index.html'), 'w') as f:
-        f.write(html)
-    print html
+    build_landing(path, pdf, description)
