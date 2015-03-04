6b04a8c9b24861c2883d9338875f856d091b0aaa
Ben-Grey <unil.greyeagle@gmail.com>
2015-03-03 18:37:47 -0800

    added multiprocessing and zipdir support, as well as several other minor but useful changes

--- pdf_extract.py
+++ pdf_extract.py
@@ -1,12 +1,17 @@
+import os
+import sys
 import bottle
 import shutil
 import hashlib
 import argparse
 import platform
 import subprocess
-import os
 from os import getcwd
 from os.path import join
+
+sys.path.insert(0, os.path.normpath('../artexin'))
+
+from artexin.pack import zipdir
 
 
 def hash_url(url):
@@ -15,35 +20,103 @@ def hash_url(url):
     return md5.hexdigest()
 
 
-def assemble_path(path, pdf):
+def assemble_path(path, pdf, pdf_dir):
     # Assemble path
-    pdf_hash = hash_url(args.pdf)
-    path = join(getcwd(), pdf_hash)
     if os.path.exists(path):
         shutil.rmtree(path)
         os.mkdir(path)
     else:
         os.mkdir(path)
-    shutil.move(join(original_dir, args.pdf), join(path, args.pdf))
+    shutil.move(join(pdf_dir, pdf), join(path, pdf))
 
 
 def get_cover(path, pdf):
     # Identify OS
     OS = platform.system()
     if OS == 'Windows':
-        subprocess.call(["convert", join(path, args.pdf) + "[0]",
-                         join(path, "cover.jpg")], shell = True)
+        subprocess.call(["convert", join(path, pdf) + "[0]",
+                         join(path, "cover.jpg")], shell=True)
     else:
-        subprocess.call("convert " + join(path, args.pdf) + "[0] " +
-                         join(path, "cover.jpg"), shell=True)
+        subprocess.call("convert '" + os.path.normpath(join(path, pdf)) +
+                        "'[0] '" + os.path.normpath(join(path, "cover.jpg")) +
+                        "'", shell=True)
 
 
 def build_landing(path, pdf, desc):
     # Build landing page
-    html = bottle.template('template', title=pdf[:-4], desc=desc)
+    html = bottle.template('template', title=pdf[:-4].title(), desc=desc)
     with open(join(path, 'index.html'), 'w') as f:
         f.write(html)
-    print html
+
+
+def worker(pdf):
+    shell = True
+    pdf_hash = hash_url(pdf)
+    path = join(pdf_dir, pdf_hash)
+
+    desc = "This PDF was gathered in bulk from KARI.org. As a result, no "\
+            "information is available for this file."
+
+    assemble_path(path, pdf, pdf_dir)
+
+    get_cover(path, pdf)
+
+    build_landing(path, pdf, desc)
+
+    zipdir(path + '.zip', path)
+
+
+def process_pdf(pdf, desc):
+    pdf_dir = os.path.dirname(os.path.abspath(pdf))
+    print(pdf_dir)
+    pdf_hash = hash_url(pdf)
+    path = join(getcwd(), pdf_hash)
+
+    assemble_path(path, pdf, pdf_dir)
+
+    get_cover(path, pdf)
+
+    build_landing(path, pdf, desc)
+
+    zipdir(path + '.zip', path)
+
+
+def process_dir(pdfdir, desc):
+    import multiprocessing
+    global pdf_dir
+    pdf_dir = pdfdir
+
+    # Prepare list to process
+    pdfs = []
+    for pdf in os.listdir(pdf_dir):
+        if pdf.endswith(".pdf"):
+            pdfs.append(pdf)
+
+    # Begin processing
+    print('Beginning multiprocessing')
+    manager = multiprocessing.Manager()
+
+    oklog = manager.list()
+    faillog = manager.list()
+
+    if not len(pdfs) > 10:
+        pool_size = len(pdfs)
+    else:
+        pool_size = 10
+
+    pool = multiprocessing.Pool(pool_size)
+    results = pool.map(worker, pdfs)
+    pool.close()
+    pool.join()
+    print('Multiprocessing complete, writing results')
+
+    # Process Results
+    failed_urls = open('failed.urls', 'w')
+    succeeded_urls = open('succeeded.urls', 'w')
+    for url in faillog:
+        failed_urls.write(url)
+    for url in oklog:
+        succeeded_urls.write(url)
 
 
 if __name__ == "__main__":
@@ -51,16 +124,16 @@ if __name__ == "__main__":
         description='Takes a pdf in and gets a cover image from the first page'
     )
     parser.add_argument('pdf', metavar='FILE', help='pdf file to extract')
-    parser.add_argument('description', metavar='FILE', default=None, help=
+    parser.add_argument('-d', help='indicates directory of pdfs',
+                        action='store_true')
+    parser.add_argument('--desc', metavar='STRING', default=None, help=
                         'description to use on pdf download page')
     args = parser.parse_args()
 
-    original_dir = getcwd()
-    bottle.TEMPLATE_PATH.insert(0, original_dir)
-    shell = True
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    bottle.TEMPLATE_PATH.insert(0, script_dir)
 
-    assemble_path(path, pdf)
-
-    get_cover(path, pdf)
-
-    build_landing(path, pdf, description)
+    if args.d:
+        process_dir(args.pdf, args.desc)
+    elif not args.d:
+        process_pdf(args.pdf, args.desc)
